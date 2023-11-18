3669149437e92bb4220b649c3e1d4d46bbaf514a
ryanwalkersyn <ryan.walkersyn@gmail.com>
2023-11-17 21:56:31 -0500

    Keyword Example

---
+++ Lesson 9/KeywordExample.py
@@ -0,0 +1,11 @@
+# new line
+print('Hello')
+print('World')
+
+# change end Keyword
+print('Hello', end='')
+print('World')
+
+#change sep Keyword
+print('cat', 'dog', 'mouse')
+print('cat', 'dog', 'mouse', sep = '-')
