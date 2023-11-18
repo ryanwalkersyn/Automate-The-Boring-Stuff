06bb34fa09bb5e78d9c6d89debda7fd091c2f1a6
ryanwalkersyn <ryan.walkersyn@gmail.com>
2023-11-17 21:23:08 -0500

    plusOne example function

--- Lesson 9/greeting.py
+++ Lesson 9/greeting.py
@@ -1,6 +1,12 @@
 def hello(name):
     print('Hello ' + name)
 
+def letterBreakdown(word):
+    print(word + ' has ' + str(len(word)) + ' letters in it.')
+
 hello('Alice')
 hello('Bob')
 
+
+letterBreakdown('Alice')
+letterBreakdown('Hello')
