1544c67a52ad166787b5ffc8440b8acdb943cf7a
ryanwalkersyn <ryan.walkersyn@gmail.com>
2023-11-17 22:12:02 -0500

    Update Error Lesson.py

--- Lesson 11/Error Lesson.py
+++ Lesson 11/Error Lesson.py
@@ -1,7 +1,9 @@
-def div42by(dividBy):
+def div42by(divideBy):
     return 42 / divideBy
+
+
 
 print(div42by(2))
 print(div42by(12))
-print(div42by(0))
-print(div42by(1))
+print(div42by(0)) # produces error - crashes and terminates the program
+print(div42by(1)) # this function call never happens
